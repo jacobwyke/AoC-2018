9859bfbdb685580dfa0f2a2781f0589716bd9e3d
Jacob Wyke <jacob.wyke@kpmg.co.uk>
2018-12-05 18:51:07 +0000

    Finishing off day 3

--- 2018/day-03/overlap.py
+++ 2018/day-03/overlap.py
@@ -1,7 +1,7 @@
 # The location of the input file
 input_file = 'input.txt'
 
-gridsize = 1500
+gridsize = 1000
 
 # read through the file and break out the data
 file = open(input_file, 'r')
@@ -24,23 +24,48 @@ for line in file:
 	}
     cuts.append(cut)
 
+def print_grid(grid):
+    for row in grid:
+        print(*row)
+        
 # create our grid
-grid = [[0] * gridsize ] * gridsize
-
-total = 0
+grid = [['0' for x in range(gridsize)] for y in range(gridsize)]
 
 for cut in cuts:
-    print cut
-    row = cut['start'][0]
-    column = cut['start'][1]
+    row = cut['start'][1]
+    column = cut['start'][0]
     width = cut['size'][0]
     height = cut['size'][1]
-    total += (width * height)
+    
     for y in range(height):
         for x in range(width):
-            print "%s, %s" % (column + x, row + y)
-            grid[column + x][row + y] += 1
+            space = grid[row + y][column + x]
             
-#print grid
-print total
-print 1000 * 1000
+            if space == '0':
+                grid[row + y][column + x] = cut['id']
+            else:
+                grid[row + y][column + x] = 'X'
+            
+count = 0            
+for x in grid:
+    count += x.count('X')
+
+print(count)
+
+for cut in cuts:
+    row = cut['start'][1]
+    column = cut['start'][0]
+    width = cut['size'][0]
+    height = cut['size'][1]
+    
+    overlaps = False
+    
+    for y in range(height):
+        for x in range(width):
+            space = grid[row + y][column + x]
+            
+            if space != cut['id']:
+                overlaps = True
+    
+    if not overlaps:
+        print(cut['id'])
